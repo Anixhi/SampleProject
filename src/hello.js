0d9b914490aa063811a6dfe3e6a62107404513c9
John Andrei Tonel <johnandreitonel@gmail.com>
2026-06-15 15:18:32 +0800

    Change server port from 3000 to 8080

--- src/hello.js
+++ src/hello.js
@@ -5,6 +5,6 @@ app.get('/', (req, res)=>{
     res.send("Hello, World! x2, just for testing");
 });  
 
-app.listen(3000, function () {
+app.listen(8080, function () {
     console.log("erver is running on port 3000");
 });
